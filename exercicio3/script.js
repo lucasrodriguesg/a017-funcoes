80784eca62d4ae43943be2fb995e6a78d0b274ff
lucasrodriguesg <lucas.rodrigues.pvd@gmail.com>
2022-09-15 07:39:58 -0300

    Exercicios do dia

---
+++ exercicio3/script.js
@@ -0,0 +1,23 @@
+function soma(num1,num2){
+    return num1 + num2
+}
+const resultadosoma = soma(+prompt("Digite um numero"),20)
+console.log ("num + 20 = " + resultadosoma)
+
+function subtração(num1,num2){
+    return num1 - num2
+}
+const resultadoSub = subtração(+prompt("Digite um numero"),20)
+console.log ("num - 20 = " + resultadoSub)
+
+function multiplicação(num1,num2){
+    return num1 * num2
+}
+const resultadoMult = multiplicação(+prompt("Digite um numero"),20)
+console.log ("num * 20 = " + resultadoMult)
+
+function divisão(num1,num2){
+    return num1/num2
+}
+const resultadoDiv = divisão(+prompt("Digite um numero"),20)
+console.log ("num / 20 = " + resultadoDiv)
